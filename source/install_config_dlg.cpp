29c588a550ba620fa1006912f9c9ed8ba6f9d212
Ravbug <ravbuganimations@gmail.com>
2023-01-18 15:02:53 -0500

    Ini parser

--- source/install_config_dlg.cpp
+++ source/install_config_dlg.cpp
@@ -3,6 +3,7 @@
 #include <fmt/format.h>
 #include "globals.h"
 #include <wx/msgdlg.h>
+#include <sstream>
 
 constexpr static const char* const ini_platform =
 #if __APPLE__
@@ -16,6 +17,61 @@ constexpr static const char* const ini_platform =
 #error Unidentified INI platform
 #endif
 
+auto parseIni (std::string_view string, uint32_t index = 0){
+    std::unordered_map<std::string_view, std::unordered_map<std::string_view, std::string_view>> inidata;
+    
+    // get the name of a section
+    auto getSectionName = [&string](uint32_t& index){
+        auto startidx = index + 1;  // advance past '['
+        while (string[index] != ']'){
+            index++;
+        }
+        return std::string_view(string.data() + startidx, index - startidx);
+    };
+    
+    auto parseSection = [&string](uint32_t& index){
+        decltype(inidata)::value_type::second_type content;
+        
+        auto getUntilChar = [&string](uint32_t& index, const char sentinel){
+            auto startidx = index;
+            while (string[index] != sentinel){
+                index++;
+            }
+            return std::string_view(string.data() + startidx,index - startidx);
+        };
+        
+        while (string[index] != '['){
+            auto name = getUntilChar(index,'=');
+            index++;    // advance past the '='
+            auto data = getUntilChar(index,'\n');
+            content[name] = data;
+            index++;    // advance past the newline
+        }
+        return content;
+    };
+    
+    // get the content of a section
+    
+    while (index < string.size()){
+        std::string_view currentSectionName = getSectionName(index);
+        char c = string[index];
+        switch (c) {
+            case '[':{
+                currentSectionName = getSectionName(index);
+            }
+            case ']':{
+                index+=2;    // advance past ']\n'
+                inidata[currentSectionName] = parseSection(index);
+            }
+                break;
+                
+            default:
+                break;
+        }
+    }
+    return inidata;
+};
+
 ConfigureInstallDlg::ConfigureInstallDlg(wxWindow* parent, const installVersionData& version) : ConfigureEditorDlgBase(parent){
     // download the INI file
     auto iniresult = fetch(fmt::format("https://download.unity3d.com/download_unity/{}/unity-{}.ini", version.hashcode,ini_platform));
@@ -23,4 +79,12 @@ ConfigureInstallDlg::ConfigureInstallDlg(wxWindow* parent, const installVersionD
         wxMessageBox("Unable to get Editor version data","Install failed",wxOK | wxICON_ERROR);
         this->Close();
     }
+    
+    // parse the ini
+   
+    auto inidata = parseIni(iniresult.text);
+    
+    this->SetTitle(fmt::format("Install {}",inidata["Unity"]["title"]));
+    
+    // populate the data table
 }
