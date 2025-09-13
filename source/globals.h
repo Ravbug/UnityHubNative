332b23156cb2bef176b7e5c5c7ec8e9a0c59530c
Ravbug <ravbug@noreply.users.github.com>
2025-09-13 12:55:40 -0700

    version and copyright

--- source/globals.h
+++ source/globals.h
@@ -14,7 +14,7 @@
 static constexpr std::string_view projectsFile = "projects.txt";
 static constexpr std::string_view editorPathsFile = "editorPaths.txt";
 static constexpr std::string_view templatePrefix = "com.unity.template";
-static constexpr std::string_view AppVersion = "v1.58";
+static constexpr std::string_view AppVersion = "v1.59";
 
 struct wxListCtrl;
 struct wxWindow;
